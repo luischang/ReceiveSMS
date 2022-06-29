dca6cb267c2e908b3f43dad9fdede060565a77a3
Luis Chang <luis.chang@iugo.com.uy>
2022-06-28 22:15:33 -0500

    Initial commit

---
+++ app/src/main/java/dev/lchang/receivesms/MainActivity.kt
@@ -0,0 +1,55 @@
+package dev.lchang.receivesms
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Telephony
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)!=PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS),111)
+        }else {
+            receiveMsg()
+        }
+
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            receiveMsg()
+    }
+
+    private fun receiveMsg() {
+        var br = object: BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT){
+                    for(sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)){
+                        Toast.makeText(applicationContext,sms.displayMessageBody,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+    }
+}
